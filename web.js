a8d037b13d0898157a7f40d895135e66b873acbb
Ubuntu <ubuntu@ip-172-31-47-199.us-west-2.compute.internal>
2013-07-21 15:18:08 +0000

    update web.js for hw4.

--- web.js
+++ web.js
@@ -1,18 +1,15 @@
 var express = require('express');
 var fs = require('fs');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
-//app.get('/', function(request, response) {
-//  response.send('Hello World 2!');
-//});
 
-var filename = 'index.html';
 app.get('/', function(request, response){
-  var data = fs.readFileSync(filename);
-  response.send(data.toString('utf8'));
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
